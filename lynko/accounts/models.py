3cb716c37bf590acc12436e640a140b6f6cc3f78
tehmeerali786 <freelanceali786@gmail.com>
2024-01-10 22:23:23 -0800

    Lynko Django SaaS

---
+++ lynko/accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from djstripe.models import Customer, Subscription
+
+
+
+# Create your models here.
+class Plan(models.Model):
+    name = models.CharField(max_length=255)
+    max_num_links = models.IntegerField()
+    
+    
+    
+class User(AbstractUser):
+    plan = models.ForeignKey(Plan, related_name='users', default=1 ,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
+    subscription = models.ForeignKey(Subscription, null=True, blank=True,on_delete=models.SET_NULL)
